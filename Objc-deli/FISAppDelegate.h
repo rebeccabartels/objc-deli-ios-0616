afb39752e6a74ecc9c7335c70a6b157aaf870c34
rebeccabartels <rebeccakbartels@icloud.com>
2016-06-27 20:29:43 -0400

    Done.

--- Objc-deli/FISAppDelegate.h
+++ Objc-deli/FISAppDelegate.h
@@ -6,10 +6,19 @@
 
 @property (strong, nonatomic) UIWindow *window;
 
-/*
- 
- *  Declare your methods here!
- 
- */
+
+
+
+
+- (NSString *)stringWithDeliLine:(NSMutableArray *)deliLine;
+
+- (void)addName:(NSString *)name
+     toDeliLine:(NSMutableArray *)deliLine;
+
+- (NSString *)serveNextCustomerInDeliLine:(NSMutableArray *)deliLine;
+
+
+
+
 
 @end
--- Objc-deli/FISAppDelegate.m
+++ Objc-deli/FISAppDelegate.m
@@ -10,10 +10,39 @@
     return YES;
 }
 
-/*
+- (NSString *)stringWithDeliLine:(NSMutableArray *)deliLine
+{
+    NSMutableString *mDeliString = [NSMutableString new];
+    
+    if (deliLine.count ==0)
+    {
+        [mDeliString appendString:@"The line is currently empty."];
+        
+    }
+        return [NSString stringWithString:mDeliString];
+    }
+    
+
+
+- (void)addName:(NSString *)name
+     toDeliLine:(NSMutableArray *)deliLine
+{
+
+    [deliLine addObject:name];
+    
+}
+
+- (NSString *)serveNextCustomerInDeliLine:(NSMutableArray *)deliLine
+{
  
- *  Define your methods here!
- 
- */
+    NSString *nextCustomer = [deliLine firstObject];
+    [deliLine removeObjectAtIndex:0];
+    return nextCustomer;
+    NSLog(@"Next Customer");
+}
+
+
+
+
 
 @end
